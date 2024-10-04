b2934827ef28fba5592a28dc66cbf35f06199a7c
Harshiling <harshiling@gmail.com>
2024-10-04 21:56:38 +0530

    Committed up to date

--- List.py
+++ List.py
@@ -1,9 +1,50 @@
-Names = ['Nandini', 'Harshitha','Durga']
+# Names = ['Nandini', 'Harshitha','Durga']
 
-print("Hello there",Names[2])
-print("Hello there",Names[1])
-print("Hello there",Names[2])
+# print("Hello there",Names[0] ,"you are cordially invited" )
+# print("Hello there",Names[1] , "you are cordially invited")
+# print("Hello there",Names[2], "you are cordially invited")
 
-name = "cholodo"
-print(f'{name}')
 
+# notAttending = (Names[0])
+# Names[0] = "Rahul"
+# print(notAttending)
+
+
+# print("Hello there",Names[0] ,"you are cordially invited" )
+# print("Hello there",Names[1] , "you are cordially invited")
+# print("Hello there",Names[2], "you are cordially invited")
+
+
+# Names.insert(2, "Kapoor")
+# []
+# Names.append("Pomo")
+# print(Names)
+
+# Names.pop()
+# Names.pop()
+# Names.pop()
+
+# del Names[1]
+# del Names[0]                                                                                                                                    
+
+# print(Names)
+
+# name = "cholodo"
+# print(f'{name}')
+
+#Sorting practice
+locations = ['Lucknow', 'Nainital' , 'OMR' , 'Madras']
+print("Original", locations)
+print("Sorted" , sorted(locations))
+print(sorted(locations , reverse=True))
+locations.reverse()
+print(locations)
+locations.reverse()
+print(locations)
+
+locations.sort()
+locations.sort(reverse=True)
+print(len(locations))
+print(locations[-1])
+
+print("original_2", locations)
