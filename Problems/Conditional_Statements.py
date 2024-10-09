d219ee40a9f700f9b2337ac26e7827e7c1476009
Harshiling <harshiling@gmail.com>
2024-10-09 23:54:12 +0530

    conditional statements

---
+++ Problems/Conditional_Statements.py
@@ -0,0 +1,15 @@
+# 1 , 2 , 3 -> A B C , any order (https://open.kattis.com/problems/abc)
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True")
+print(car == 'subaru')
+
+sweet = "Chd"
+print("Is sweet == 'Jamun'? I predict True")
+print(sweet.lower() == 'chd' or sweet.upper() == "dd")
+
+ 
+
+
+
+
